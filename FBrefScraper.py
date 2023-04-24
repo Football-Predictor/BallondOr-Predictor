2acd318fbad5958ae85b2b3fbde1d1e7d3a454e2
Steven Tohme <steventohme59@gmail.com>
2023-04-24 01:28:12 -0400

    added URLs of top 5 leagues

--- FBrefScraper.py
+++ FBrefScraper.py
@@ -1,3 +1,13 @@
+
+LEAGUE_URLS = {
+    "Premier League": "https://fbref.com/en/comps/9/",
+    "Bundesliga": "https://fbref.com/en/comps/20/",
+    "La Liga": "https://fbref.com/en/comps/12/",
+    "Serie A": "https://fbref.com/en/comps/11/",
+    "Ligue 1": "https://fbref.com/en/comps/13/"
+}
+    
+
 class FBrefScraper:
     def __init__(self, leagues, seasons):
         self.leagues = leagues
