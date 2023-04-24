477abad4a10817f60e57f4bc1035fe4f69a35343
Steven Tohme <steventohme59@gmail.com>
2023-04-24 01:38:25 -0400

    implemented player data scraper

--- FBrefScraper.py
+++ FBrefScraper.py
@@ -1,3 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+import sys, getopt
+import csv
 
 LEAGUE_URLS = {
     "Premier League": "https://fbref.com/en/comps/9/",
@@ -12,3 +18,18 @@ class FBrefScraper:
     def __init__(self, leagues, seasons):
         self.leagues = leagues
         self.seasons = seasons
+    
+    def scrapePlayers(self, csvPath=None):
+        outfieldStats = pd.DataFrame()
+        for league in self.leagues:
+            for season in self.seasons:
+                print(f"Scraping {league}, {season - 1}/{season}...")
+                url = LEAGUE_URLS[league] + f"{season - 1}-{season}"
+                # TODO implement getPlayerData
+                outfieldStatsLeague = getPlayerData(url)
+                outfieldStatsLeague["season"] = season
+                outfieldStats = outfieldStats.append(outfieldStatsLeague, ignore_index=True)
+        if csvPath:
+            outfieldStats.to_csv(csvPath, index=False)
+        return outfieldStats        
+
