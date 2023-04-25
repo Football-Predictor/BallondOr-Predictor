131d8bcd7a3ae70d47645c3c89da61c5bd64755a
Steven Tohme <steventohme59@gmail.com>
2023-04-24 21:27:55 -0400

    remove single quotes

--- FBrefScraper.py
+++ FBrefScraper.py
@@ -15,15 +15,15 @@ LEAGUE_URLS = {
 
 # Stats to be scraped from FBref.com, key is the category, value is a list of stat names to be pulled
 STATS = {
-    "stats": ['player', 'nationality', 'position', 'team', 'age', 'birth_year', 'games', 'games_starts', 'minutes', 'minutes_90s', 'goals', 'assists', 'goals_assists', 'goals_pens', 'pens_made', 'pens_att', 'cards_yellow', 'cards_red', 'xg', 'npxg', 'xg_assist', 'npxg_xg_assist', 'progressive_carries', 'progressive_passes', 'progressive_passes_received', 'goals_per90', 'assists_per90', 'goals_assists_per90', 'goals_pens_per90', 'goals_assists_pens_per90', 'xg_per90', 'xg_assist_per90', 'xg_xg_assist_per90', 'npxg_per90', 'npxg_xg_assist_per90'],
-    "shooting": ['minutes_90s', 'goals', 'shots', 'shots_on_target', 'shots_on_target_pct', 'shots_per90', 'shots_on_target_per90', 'goals_per_shot', 'goals_per_shot_on_target', 'average_shot_distance', 'shots_free_kicks', 'pens_made', 'pens_att', 'xg', 'npxg', 'npxg_per_shot', 'xg_net', 'npxg_net'],
-    "passing":['passes_completed', 'passes', 'passes_pct', 'passes_total_distance', 'passes_progressive_distance', 'passes_completed_short', 'passes_short', 'passes_pct_short', 'passes_completed_medium', 'passes_medium', 'passes_pct_medium', 'passes_completed_long', 'passes_long', 'passes_pct_long', 'assists', 'xg_assist', 'pass_xa', 'xg_assist_net', 'assisted_shots', 'passes_into_final_third', 'passes_into_penalty_area', 'crosses_into_penalty_area', 'progressive_passes'],
-    "passing_types": ['passes', 'passes_live', 'passes_dead', 'passes_free_kicks', 'through_balls', 'passes_switches', 'crosses', 'throw_ins', 'corner_kicks', 'corner_kicks_in', 'corner_kicks_out', 'corner_kicks_straight', 'passes_completed', 'passes_offsides', 'passes_blocked'],
+    "stats": ["player", "nationality", "position", "team", "age", "birth_year", "games", "games_starts", "minutes", "minutes_90s", "goals", "assists", "goals_assists", "goals_pens", "pens_made", "pens_att", "cards_yellow", "cards_red", "xg", "npxg", "xg_assist", "npxg_xg_assist", "progressive_carries", "progressive_passes", "progressive_passes_received", "goals_per90", "assists_per90", "goals_assists_per90", "goals_pens_per90", "goals_assists_pens_per90", "xg_per90", "xg_assist_per90", "xg_xg_assist_per90", "npxg_per90", "npxg_xg_assist_per90"],
+    "shooting": ["minutes_90s", "goals", "shots", "shots_on_target", "shots_on_target_pct", "shots_per90", "shots_on_target_per90", "goals_per_shot", "goals_per_shot_on_target", "average_shot_distance", "shots_free_kicks", "pens_made", "pens_att", "xg", "npxg", "npxg_per_shot", "xg_net", "npxg_net"],
+    "passing":["passes_completed", "passes", "passes_pct", "passes_total_distance", "passes_progressive_distance", "passes_completed_short", "passes_short", "passes_pct_short", "passes_completed_medium", "passes_medium", "passes_pct_medium", "passes_completed_long", "passes_long", "passes_pct_long", "assists", "xg_assist", "pass_xa", "xg_assist_net", "assisted_shots", "passes_into_final_third", "passes_into_penalty_area", "crosses_into_penalty_area", "progressive_passes"],
+    "passing_types": ["passes", "passes_live", "passes_dead", "passes_free_kicks", "through_balls", "passes_switches", "crosses", "throw_ins", "corner_kicks", "corner_kicks_in", "corner_kicks_out", "corner_kicks_straight", "passes_completed", "passes_offsides", "passes_blocked"],
     # goal and shot creation
-    "gca": ['sca', 'sca_per90', 'sca_passes_live', 'sca_passes_dead', 'sca_take_ons', 'sca_shots', 'sca_fouled', 'sca_defense', 'gca', 'gca_per90', 'gca_passes_live', 'gca_passes_dead', 'gca_take_ons', 'gca_shots', 'gca_fouled', 'gca_defense'],
-    "defense": ['tackles', 'tackles_won', 'tackles_def_3rd', 'tackles_mid_3rd', 'tackles_att_3rd', 'challenge_tackles', 'challenges', 'challenge_tackles_pct', 'challenges_lost', 'blocks', 'blocked_shots', 'blocked_passes', 'interceptions', 'tackles_interceptions', 'clearances', 'errors'],
-    "possession": ['touches', 'touches_def_pen_area', 'touches_def_3rd', 'touches_mid_3rd', 'touches_att_3rd', 'touches_att_pen_area', 'touches_live_ball', 'take_ons', 'take_ons_won', 'take_ons_won_pct', 'take_ons_tackled', 'take_ons_tackled_pct', 'carries', 'carries_distance', 'carries_progressive_distance', 'progressive_carries', 'carries_into_final_third', 'carries_into_penalty_area', 'miscontrols', 'dispossessed', 'passes_received', 'progressive_passes_received'],
-    "misc": ['cards_yellow', 'cards_red', 'cards_yellow_red', 'fouls', 'fouled', 'offsides', 'crosses', 'interceptions', 'tackles_won', 'pens_won', 'pens_conceded', 'own_goals', 'ball_recoveries', 'aerials_won', 'aerials_lost', 'aerials_won_pct']
+    "gca": ["sca", "sca_per90", "sca_passes_live", "sca_passes_dead", "sca_take_ons", "sca_shots", "sca_fouled", "sca_defense", "gca", "gca_per90", "gca_passes_live", "gca_passes_dead", "gca_take_ons", "gca_shots", "gca_fouled", "gca_defense"],
+    "defense": ["tackles", "tackles_won", "tackles_def_3rd", "tackles_mid_3rd", "tackles_att_3rd", "challenge_tackles", "challenges", "challenge_tackles_pct", "challenges_lost", "blocks", "blocked_shots", "blocked_passes", "interceptions", "tackles_interceptions", "clearances", "errors"],
+    "possession": ["touches", "touches_def_pen_area", "touches_def_3rd", "touches_mid_3rd", "touches_att_3rd", "touches_att_pen_area", "touches_live_ball", "take_ons", "take_ons_won", "take_ons_won_pct", "take_ons_tackled", "take_ons_tackled_pct", "carries", "carries_distance", "carries_progressive_distance", "progressive_carries", "carries_into_final_third", "carries_into_penalty_area", "miscontrols", "dispossessed", "passes_received", "progressive_passes_received"],
+    "misc": ["cards_yellow", "cards_red", "cards_yellow_red", "fouls", "fouled", "offsides", "crosses", "interceptions", "tackles_won", "pens_won", "pens_conceded", "own_goals", "ball_recoveries", "aerials_won", "aerials_lost", "aerials_won_pct"]
 }
 
 
@@ -33,7 +33,7 @@ def categoryFrame(category, url):
         """Returns the table containing player stats"""
         res = requests.get(url)
         comm = re.compile("<!--|-->")
-        soup = BeautifulSoup(comm.sub("",res.text),'lxml')
+        soup = BeautifulSoup(comm.sub("",res.text),"lxml")
         allTables = soup.findAll("tbody")
         playerTable = allTables[2]
         return playerTable
@@ -43,9 +43,9 @@ def categoryFrame(category, url):
         table containing player stats"""
         dfDict = {}
         features = STATS[category]
-        rows = playerTable.find_all('tr')
+        rows = playerTable.find_all("tr")
         for row in rows:
-            if row.find('th',{"scope":"row"}):
+            if row.find("th",{"scope":"row"}):
                 for f in features:
                     cell = row.find("td",{"data-stat": f})
                     text = cell.text.strip().encode().decode("utf-8")
