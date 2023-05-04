1152be87267f88c2ddb79c24bec6663cd23d0011
Steven Tohme <steventohme59@gmail.com>
2023-05-04 14:15:37 -0400

    update csvtomongo to be class

--- CSVtoMongo.py
+++ CSVtoMongo.py
@@ -4,19 +4,19 @@ import pandas as pd
 # This will be removed for final version, this is our connection string to mongoDB
 CONNECTIONSTRING = r"mongodb+srv://anduarielsivansteven:aass123!@ballondor.csw3klm.mongodb.net/?retryWrites=true&w=majority"
 
-def addCSVToMongoDB(csvPath, connectionString, collectionName="BallondOrPredictor",dbName="FootballPredictor"):
-    """Adds csv data to a mongoDB database"""
-    client = MongoClient(connectionString)
-    db = client[dbName]
-    collection = db[collectionName]
-    df = pd.read_csv(csvPath)
-    data = df.to_dict(orient='records')
-    collection.insert_many(data)
+class MongoDB:
+    """A class to handle all mongoDB interactions"""
+    def __init__(self, connectionString, collectionName="BallondOrPredictor",dbName="FootballPredictor"):
+        self.client = MongoClient(connectionString)
+        self.db = self.client[dbName]
+        self.collection = self.db[collectionName]
 
+    def addCSVToMongoDB(self, csvPath):
+        """Adds csv data to a mongoDB database"""
+        df = pd.read_csv(csvPath)
+        data = df.to_dict(orient='records')
+        self.collection.insert_many(data)
 
-def clearMongoDB(connectionString, collectionName="BallondOrPredictor",dbName="FootballPredictor"):
-    """Clears a mongoDB database"""
-    client = MongoClient(connectionString)
-    db = client[dbName]
-    collection = db[collectionName]
-    collection.delete_many({})
+    def clearMongoDB(self):
+        """Clears a mongoDB database"""
+        self.collection.delete_many({})
